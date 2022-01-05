37266d4f66854cd6a2e5d95565a55df26c39eef9
saxxone <saxxone17@gmail.com>
2022-01-05 23:39:02 +0100

    added og image

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,7 @@ export default {
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'description', name: 'description', content: ''},
+      {hid: 'og:image', property: 'og:image', content: '/review.jpeg'},
       {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
