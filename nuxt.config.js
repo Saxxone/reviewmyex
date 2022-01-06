11d79fd8a9d21ee4a8edc7a7dc609e3e9c2241f8
saxxone <saxxone17@gmail.com>
2022-01-06 11:04:12 +0100

    gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,11 @@ export default {
 
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: 'Review your past dates and exes, and help others know what to expect.'},
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Review your past dates and exes, and help others know what to expect.'
+      },
       {name: 'format-detection', content: 'telephone=no'},
       {name: 'twitter:description', content: 'Review your past dates and exes, and help others know what to expect.'},
       {name: 'twitter:title', content: 'Review My Ex'},
@@ -21,6 +25,10 @@ export default {
         href: 'https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
         rel: 'stylesheet'
       },
+    ],
+    script: [
+      {async: 'async', src: "https://www.googletagmanager.com/gtag/js?id=G-LD221QEGHP"},
+      {src: "/gtag.js"},
     ]
   },
   server: {port: 5000},
